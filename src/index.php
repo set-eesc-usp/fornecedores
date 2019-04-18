7fa820b29978668913d6c14e828ab702980d8b76
Wlaldemir Mazzari <wmazzari@sc.usp.br>
2019-04-18 09:15:17 -0300

    lista fornecedores

--- src/index.php
+++ src/index.php
@@ -4,5 +4,18 @@
  </head>
  <body>
    Lista de Fornecedores
+
+   <ol>
+  <li>Fornecedor 01</li>
+  <li>Fornecedor 02</li>
+  <li>Fornecedor 03</li>
+  <li>Fornecedor 04</li>
+  <li>Fornecedor 05</li>
+  <li>Fornecedor 06</li>
+  <li>Fornecedor 07</li>
+  <li>Fornecedor 08</li>
+  <li>Fornecedor 09</li>
+  <li>Fornecedor 10</li>
+</ol>
  </body>
 </html>
